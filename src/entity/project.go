afd2ae94f0ac00e0155f3bbe1a6f30f4ba6cb938
Guilherme Alves <guilherme.alves3340@gmail.com>
2022-03-25 12:14:36 -0300

    Refatorando algumas variaveis como constantes globais

--- src/entity/project.go
+++ src/entity/project.go
@@ -10,7 +10,13 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (
+	alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	ROUND_LIMIT = 60
+	secretLength = 32
+	credentialLegthPart = 32
+	keyLegth = 50
+)
 
 type Project struct {
 	Name         string             `bson:"name,omitempty"`
@@ -33,20 +39,17 @@ func NewProject() *Project {
 
 func (p *Project) GenerateKey() string {
 	uid, _ := uuid.NewUUID()
-	keyLegth := 50
 	key := uuid.NewSHA1(uid, []byte(p.generateStringRandom(uint(keyLegth))))
 	return key.String()
 }
 
 func (p *Project) GenerateCredential() string {
-	credentialLegthPart := 32
 	firstPartCredential := p.generateStringRandom(uint(credentialLegthPart))
 	lastPartCredential := p.generateStringRandom(uint(credentialLegthPart))
 	return firstPartCredential + "-" + lastPartCredential
 }
 
 func (p *Project) GenerateSecret() string {
-	secretLength := 32
 	return p.generateStringRandom(uint(secretLength))
 }
 
@@ -62,7 +65,7 @@ func (p *Project) generateStringRandom(length uint) string {
 }
 
 func (p *Project) IsValid() error {
-	if p.RoundHash < 1 || p.RoundHash > 60 {
+	if p.RoundHash < 1 || p.RoundHash > uint(ROUND_LIMIT) {
 		return errors.New("Round Hash over limit permit")
 	}
 
